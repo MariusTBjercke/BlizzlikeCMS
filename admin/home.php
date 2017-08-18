2485e6b6494fb74d2cc2a86fc74732d94e452b6d
Marius Bjercke <marius@coretrek.no>
2017-08-18 09:29:29 +0200

    Added new menu item in admin panel

--- admin/home.php
+++ admin/home.php
@@ -6,5 +6,6 @@
         <li><span class="glyphicon glyphicon-user"></span> <a href="admin.php?page=edit_accounts">Edit accounts</a></li>
         <li><span class="glyphicon glyphicon-pencil"></span> <a href="admin.php?page=edit_posts">Edit posts</a></li>
         <li><span class="glyphicon glyphicon-flag"></span> <a href="admin.php?page=edit_gamemasters">Edit list of gamemasters</a></li>
+        <li><span class="glyphicon glyphicon-wrench"></span> <a href="admin.php?page=edit_settings">Edit website settings</a></li>
     </ul>
 </div>
