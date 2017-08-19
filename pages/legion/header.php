559ed4fdc52f19bba9b7ad29a2e2218e04f36dc0
Marius Bjercke <marius@bjerckemedia.no>
2017-08-19 19:38:44 +0200

    Finished legion nav style

--- pages/legion/header.php
+++ pages/legion/header.php
@@ -5,6 +5,7 @@
             <ul class="pull-right">
                 <li><a href="#">Support</a></li>
                 <li><a href="#">My Account</a></li>
+                <li><a href="#">Administration</a></li>
             </ul>
         </div>
     </div>
@@ -12,13 +13,22 @@
 
 <div class="container">
     <div class="navbar-legion">
-    <a href="index.php" title="<?php echo $servername; ?>"><div class="top-logo"></div></a>
+        <a href="index.php" title="<?php echo $servername; ?>"><div class="top-logo"></div></a>
+        <div class="navbar-nav">
+            <div class="navbar-menuitem">Home</div>
+            <div class="navbar-menuitem">Register</div>
+            <div class="navbar-menuitem">Online players</div>
+            <div class="navbar-menuitem">How to</div>
+            <div class="navbar-menuitem">Armory</div>
+            <div class="navbar-menuitem">Gallery</div>
+        </div>
     </div>
 </div>
 
 <div class="container">
     <div class="front-banner">
         <?php
+        $site = new Site();
         echo $site->getLatestPost();
         ?>
     </div>
