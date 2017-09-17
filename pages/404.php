0bf5562163c8f9c54edb67bc40d7572de0c8e850
Marius Bjercke <marius@bjerckemedia.no>
2017-09-17 11:48:43 +0200

    404 update

--- pages/404.php
+++ pages/404.php
@@ -1,6 +1,2 @@
-<div class="fourofour-stormtrooper"></div>
-
 <h1>404</h1>
-<h2>THIS IS NOT THE PAGE YOU'RE LOOKING FOR.</h2>
-<h3>Move along...</h3>
-<h3>Move along...</h3>
+<h2>Unfortunately we could not find the page you are looking for..</h2>
