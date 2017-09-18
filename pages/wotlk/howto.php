7f54aaa01a7c80f7fa8850f640326d4c4750bebc
Marius Bjercke <marius@coretrek.no>
2017-09-18 09:44:20 +0200

    Don't display download link if empty on howto page

--- pages/wotlk/howto.php
+++ pages/wotlk/howto.php
@@ -1,12 +1,17 @@
 <div class="howto-page">
 
     <h3>1. <a href="register.php">Register for an account</a> if you haven't already.</h3>
-
-    <h3>2. If you haven't already, download the Wrath of the Lich King client. <strong>Download link: </strong><a
-                href="<?php echo $wotlk_downloadlink; ?>">WotLK Client (This server, fast connection.)</a></h3>
-    <h3>3. If you've downloaded the client from above, you can skip the next steps and <sropen <strong>wow.exe
-            to start playing right away.</strong></h3>
-    <p>or...</p>
+	<?php
+	if (strlen($wotlk_downloadlink) > 0) {
+		?>
+        <h3>2. If you haven't already, download the Wrath of the Lich King client. <strong>Download link: </strong><a
+                    href="<?php echo $wotlk_downloadlink; ?>">WotLK Client (This server, fast connection.)</a></h3>
+        <h3>3. If you've downloaded the client from above, you can skip the next steps and <sropen <strong>wow.exe
+                to start playing right away.</strong></h3>
+        <p>or...</p>
+		<?php
+	}
+	?>
     <h3>4. Go to "../World of Warcraft/Data/enUS/realmlist.wtf" or "../World of Warcraft/Data/enGb/realmlist.wtf" file.</h3>
     <img src="../../img/realmlist.png">
     <h3>5. Open <strong>realmlist.wtf</strong> with Notepad or any text editor.</h3>
