33d4c34858b920bc747fbf8fec207460dbc3d190
Marius Bjercke <marius@coretrek.no>
2017-09-18 09:52:35 +0200

    Removed analytics script

--- header.php
+++ header.php
@@ -27,16 +27,6 @@ $activeplayersResult = $mysqli->query("SELECT * FROM characters");
     ?>
 </head>
 <body>
-<script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-    ga('create', 'UA-55335908-4', 'auto');
-    ga('send', 'pageview');
-
-</script>
 
 <?php
 include 'pages/' . $theme_name . '/header.php';
