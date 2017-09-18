c1e1f2c1e552152ab9e243c05a8b49c87985dbd3
Marius Bjercke <marius@coretrek.no>
2017-09-18 08:41:20 +0200

    Made legion default theme

--- pages/install.php
+++ pages/install.php
@@ -98,7 +98,7 @@ $installed = true;
     $mysqli->query("CREATE TABLE posts (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255) NOT NULL, content TEXT NOT NULL, poster_id INT(255) NOT NULL)");
     $mysqli->query("INSERT INTO posts (title, content, poster_id) VALUES ('Example Post', 'This post can be edited from the administration panel. You can also add new posts from there.', '1')");
     $mysqli->query("CREATE TABLE config (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, servername VARCHAR(255) NOT NULL, serveraddress VARCHAR(255) NOT NULL, worldport VARCHAR(255) NOT NULL, contact VARCHAR(255) NOT NULL, youtube VARCHAR(255), download VARCHAR(255), theme INT(11) NOT NULL)");
-    $mysqli->query("INSERT INTO config (servername, serveraddress, worldport, contact, youtube, download, theme) VALUES ('$servername', '$serveraddress', '$worldport', 'contact@example.com', '$youtube', '$download', '1')");
+    $mysqli->query("INSERT INTO config (servername, serveraddress, worldport, contact, youtube, download, theme) VALUES ('$servername', '$serveraddress', '$worldport', 'contact@example.com', '$youtube', '$download', '2')");
     $mysqli->query("CREATE TABLE gamemasters (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, charname VARCHAR(255) NOT NULL)");
     $mysqli->query("INSERT INTO gamemasters (charname) VALUES ('Example')");
 	$mysqli->query("CREATE TABLE gallery (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255) NOT NULL, url VARCHAR(255) NOT NULL)");
