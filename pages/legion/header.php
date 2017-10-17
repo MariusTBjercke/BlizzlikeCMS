942971c7e83cd5995da46a58bb9705714a59e520
Marius Bjercke <marius@bjerckemedia.no>
2017-10-18 00:00:40 +0200

    Added new active class for mobile menu

--- pages/legion/header.php
+++ pages/legion/header.php
@@ -18,13 +18,13 @@
             <button type="button" title="Open menu" id="toggleNavigation" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                 <div class="toggledMenu">
                     <ul id="toggledMenuUl">
-                        <li><a href="index.php">Home</a></li>
-                        <li><a href="register.php">Register</a></li>
-                        <li><a href="onlineplayers.php">Who's online</a></li>
-                        <li><a href="howto.php">How to</a></li>
-                        <li><a href="armory.php">Armory</a></li>
-                        <li><a href="gallery.php">Gallery</a></li>
-                        <li><a href="gamemasters.php">Staff</a></li>
+                        <li class="<?php if ($page == 'home') { echo 'mobilemenu-active'; } ?>"><a href="index.php"><span class="glyphicon glyphicon-home"></span> Home</a></li>
+                        <li class="<?php if ($page == 'register') { echo 'mobilemenu-active'; } ?>"><a href="register.php"><span class="glyphicon glyphicon-pencil"></span> Register</a></li>
+                        <li class="<?php if ($page == 'onlineplayers') { echo 'mobilemenu-active'; } ?>"><a href="onlineplayers.php"><span class="glyphicon glyphicon-user"></span> Who's online</a></li>
+                        <li class="<?php if ($page == 'howto') { echo 'mobilemenu-active'; } ?>"><a href="howto.php"><span class="glyphicon glyphicon-question-sign"></span> How to</a></li>
+                        <li class="<?php if ($page == 'armory') { echo 'mobilemenu-active'; } ?>"><a href="armory.php"><span class="glyphicon glyphicon-user"></span> Armory</a></li>
+                        <li class="<?php if ($page == 'gallery') { echo 'mobilemenu-active'; } ?>"><a href="gallery.php"><span class="glyphicon glyphicon-picture"></span> Gallery</a></li>
+                        <li class="<?php if ($page == 'gamemasters') { echo 'mobilemenu-active'; } ?>"><a href="gamemasters.php"><span class="glyphicon glyphicon-exclamation-sign"></span> Staff</a></li>
                     </ul>
                 </div>
                 <span class="icon-bar"></span>
