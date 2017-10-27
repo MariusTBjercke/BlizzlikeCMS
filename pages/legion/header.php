744a5ef1be38a303423f581aa41e9653d0b3b098
Marius Bjercke <marius@coretrek.no>
2017-10-27 09:19:20 +0200

    Removed forum link because it's not active yet

--- pages/legion/header.php
+++ pages/legion/header.php
@@ -21,7 +21,7 @@
                         <li class="<?php if ($page == 'home') { echo 'mobilemenu-active'; } ?>"><a href="index.php"><span class="glyphicon glyphicon-home"></span> Home</a></li>
                         <li class="<?php if ($page == 'register') { echo 'mobilemenu-active'; } ?>"><a href="register.php"><span class="glyphicon glyphicon-pencil"></span> Register</a></li>
                         <li class="<?php if ($page == 'onlineplayers') { echo 'mobilemenu-active'; } ?>"><a href="onlineplayers.php"><span class="glyphicon glyphicon-user"></span> Who's online</a></li>
-                        <li class="<?php if ($page == 'forum') { echo 'mobilemenu-active'; } ?>"><a href="forum.php"><span class="glyphicon glyphicon-user"></span> Forum</a></li>
+<!--                        <li class="--><?php //if ($page == 'forum') { echo 'mobilemenu-active'; } ?><!--"><a href="forum.php"><span class="glyphicon glyphicon-user"></span> Forum</a></li>-->
                         <li class="<?php if ($page == 'howto') { echo 'mobilemenu-active'; } ?>"><a href="howto.php"><span class="glyphicon glyphicon-question-sign"></span> How to</a></li>
                         <li class="<?php if ($page == 'armory') { echo 'mobilemenu-active'; } ?>"><a href="armory.php"><span class="glyphicon glyphicon-user"></span> Armory</a></li>
                         <li class="<?php if ($page == 'gallery') { echo 'mobilemenu-active'; } ?>"><a href="gallery.php"><span class="glyphicon glyphicon-picture"></span> Gallery</a></li>
@@ -37,7 +37,7 @@
                 <div class="navbar-menuitem <?php if ($page == 'home') { echo 'menuitem-active'; } ?>"><a href="index.php">Home</a></div>
                 <div class="navbar-menuitem <?php if ($page == 'register') { echo 'menuitem-active'; } ?>"><a href="register.php">Register</a></div>
                 <div class="navbar-menuitem <?php if ($page == 'onlineplayers') { echo 'menuitem-active'; } ?>"><a href="onlineplayers.php">Online players</a></div>
-                <div class="navbar-menuitem <?php if ($page == 'forum') { echo 'menuitem-active'; } ?>"><a href="forum.php">Forum</a></div>
+<!--                <div class="navbar-menuitem --><?php //if ($page == 'forum') { echo 'menuitem-active'; } ?><!--"><a href="forum.php">Forum</a></div>-->
                 <div class="navbar-menuitem <?php if ($page == 'howto') { echo 'menuitem-active'; } ?>"><a href="howto.php">How to</a></div>
                 <div class="navbar-menuitem <?php if ($page == 'armory') { echo 'menuitem-active'; } ?>"><a href="armory.php">Armory</a></div>
                 <div class="navbar-menuitem <?php if ($page == 'gallery') { echo 'menuitem-active'; } ?>"><a href="gallery.php">Gallery</a></div>
