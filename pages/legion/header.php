71a7dcdbb011152abc574ccfa9066ba5c883b86b
Marius Bjercke <marius@bjerckemedia.no>
2017-12-09 13:02:22 +0100

    Legion menu fix

--- pages/legion/header.php
+++ pages/legion/header.php
@@ -18,14 +18,14 @@
             <button type="button" title="Open menu" id="toggleNavigation" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                 <div class="toggledMenu">
                     <ul id="toggledMenuUl">
-                        <li class="<?php if ($page == 'home') { echo 'mobilemenu-active'; } ?>"><a hawdref="index.php"><span class="glyphicon glyphicon-home"></span> Home</a></li>
-                        <li class="<?php if ($page == 'register') { echo 'mobilemenu-active'; } ?>"><a href="register.php"><span class="glyphicon glyphicon-pencil"></span> Register</a></li>
-                        <li class="<?php if ($page == 'onlineplayers') { echo 'mobilemenu-active'; } ?>"><a href="onlineplayers.php"><span class="glyphicon glyphicon-user"></span> Who's online</a></li>
-<!--                        <li class="--><?php //if ($page == 'forum') { echo 'mobilemenu-active'; } ?><!--"><a href="forum.php"><span class="glyphicon glyphicon-user"></span> Forum</a></li>-->
-                        <li class="<?php if ($page == 'howto') { echo 'mobilemenu-active'; } ?>"><a href="howto.php"><span class="glyphicon glyphicon-question-sign"></span> How to</a></li>
-                        <li class="<?php if ($page == 'armory') { echo 'mobilemenu-active'; } ?>"><a href="armory.php"><span class="glyphicon glyphicon-user"></span> Armory</a></li>
-                        <li class="<?php if ($page == 'gallery') { echo 'mobilemenu-active'; } ?>"><a href="gallery.php"><span class="glyphicon glyphicon-picture"></span> Gallery</a></li>
-                        <li class="<?php if ($page == 'gamemasters') { echo 'mobilemenu-active'; } ?>"><a href="gamemasters.php"><span class="glyphicon glyphicon-exclamation-sign"></span> Staff</a></li>
+                        <li class="<?php if ($page == 'home') { echo 'mobilemenu-active'; } ?>"><a href="index.php"><i class="fa fa-home" aria-hidden="true"></i> Home</a></li>
+                        <li class="<?php if ($page == 'register') { echo 'mobilemenu-active'; } ?>"><a href="register.php"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Register</a></li>
+                        <li class="<?php if ($page == 'onlineplayers') { echo 'mobilemenu-active'; } ?>"><a href="onlineplayers.php"><i class="fa fa-users" aria-hidden="true"></i> Who's online</a></li>
+                        <!--                        <li class="--><?php //if ($page == 'forum') { echo 'mobilemenu-active'; } ?><!--"><a href="forum.php"><span class="glyphicon glyphicon-user"></span> Forum</a></li>-->
+                        <li class="<?php if ($page == 'howto') { echo 'mobilemenu-active'; } ?>"><a href="howto.php"><i class="fa fa-question-circle" aria-hidden="true"></i> How to</a></li>
+                        <li class="<?php if ($page == 'armory') { echo 'mobilemenu-active'; } ?>"><a href="armory.php"><i class="fa fa-universal-access" aria-hidden="true"></i> Armory</a></li>
+                        <li class="<?php if ($page == 'gallery') { echo 'mobilemenu-active'; } ?>"><a href="gallery.php"><i class="fa fa-file-image-o" aria-hidden="true"></i> Gallery</a></li>
+                        <li class="<?php if ($page == 'gamemasters') { echo 'mobilemenu-active'; } ?>"><a href="gamemasters.php"><i class="fa fa-star blinking" aria-hidden="true"></i> Staff</a></li>
                     </ul>
                 </div>
                 <span class="icon-bar"></span>
@@ -35,13 +35,13 @@
 
             <div class="navbar-list">
                 <div class="navbar-menuitem <?php if ($page == 'home') { echo 'menuitem-active'; } ?>"><a href="index.php"><i class="fa fa-home" aria-hidden="true"></i> Home</a></div>
-                <div class="navbar-menuitem <?php if ($page == 'register') { echo 'menuitem-active'; } ?>"><a href="register.php">Register</a></div>
-                <div class="navbar-menuitem <?php if ($page == 'onlineplayers') { echo 'menuitem-active'; } ?>"><a href="onlineplayers.php">Online players</a></div>
-<!--                <div class="navbar-menuitem --><?php //if ($page == 'forum') { echo 'menuitem-active'; } ?><!--"><a href="forum.php">Forum</a></div>-->
-                <div class="navbar-menuitem <?php if ($page == 'howto') { echo 'menuitem-active'; } ?>"><a href="howto.php">How to</a></div>
-                <div class="navbar-menuitem <?php if ($page == 'armory') { echo 'menuitem-active'; } ?>"><a href="armory.php">Armory</a></div>
-                <div class="navbar-menuitem <?php if ($page == 'gallery') { echo 'menuitem-active'; } ?>"><a href="gallery.php">Gallery</a></div>
-                <div class="navbar-menuitem <?php if ($page == 'gamemasters') { echo 'menuitem-active'; } ?>"><a href="gamemasters.php">Staff</a></div>
+                <div class="navbar-menuitem <?php if ($page == 'register') { echo 'menuitem-active'; } ?>"><a href="register.php"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Register</a></div>
+                <div class="navbar-menuitem <?php if ($page == 'onlineplayers') { echo 'menuitem-active'; } ?>"><a href="onlineplayers.php"><i class="fa fa-users" aria-hidden="true"></i> Online players</a></div>
+                <!--                <div class="navbar-menuitem --><?php //if ($page == 'forum') { echo 'menuitem-active'; } ?><!--"><a href="forum.php">Forum</a></div>-->
+                <div class="navbar-menuitem <?php if ($page == 'howto') { echo 'menuitem-active'; } ?>"><a href="howto.php"><i class="fa fa-question-circle" aria-hidden="true"></i> How to</a></div>
+                <div class="navbar-menuitem <?php if ($page == 'armory') { echo 'menuitem-active'; } ?>"><a href="armory.php"><i class="fa fa-universal-access" aria-hidden="true"></i> Armory</a></div>
+                <div class="navbar-menuitem <?php if ($page == 'gallery') { echo 'menuitem-active'; } ?>"><a href="gallery.php"><i class="fa fa-file-image-o" aria-hidden="true"></i> Gallery</a></div>
+                <div class="navbar-menuitem <?php if ($page == 'gamemasters') { echo 'menuitem-active'; } ?>"><a href="gamemasters.php"><i class="fa fa-star blinking" aria-hidden="true"></i> Staff</a></div>
             </div>
 
         </div>
