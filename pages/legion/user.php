933fb572bd1d72e53a63055c85e9b13a1f646138
Marius Bjercke <marius@bjerckemedia.no>
2018-01-30 12:03:32 +0100

    More modifications to user page

--- pages/legion/user.php
+++ pages/legion/user.php
@@ -5,14 +5,31 @@ $account->retrieveAccount();
 
 <div class="user_page">
 
-    <h1>Welcome, <?php echo $account->getName(); ?></h1>
+    <div class="row">
 
-    <p>Avatar (Hover to change)</p>
-    <div class="user-avatar">
-        <div class="change-avatar">
-            <span><a href="#">Change avatar?</a></span>
+        <div class="col-sm-6">
+
+            <h1>Welcome, <?php echo $account->getName(); ?></h1>
+
+            <p>Avatar (Hover to change)</p>
+            <div class="user-avatar">
+                <div class="change-avatar">
+                    <span><a href="#">Change avatar?</a></span>
+                </div>
+                <img src="../../img/avatars/no-avatar.png" alt="Avatar">
+            </div>
+
         </div>
-        <img src="../../img/avatars/no-avatar.png" alt="Avatar">
+
+        <div class="col-sm-6 text-right user-facts">
+            <h1>Your facts:</h1>
+            <ul>
+                <li><span>Email: </span><?php echo $account->getEmail(); ?></li>
+                <li><span>Last login: </span><?php echo $account->getLastLogin(); ?></li>
+                <li><span>Last IP: </span><?php echo $account->getLastIP(); ?></li>
+            </ul>
+        </div>
+
     </div>
 
 </div>
