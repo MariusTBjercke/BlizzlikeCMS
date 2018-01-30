43e0069df7e4bdff798e0ffefa03b5366c1182d5
Marius Bjercke <marius@bjerckemedia.no>
2018-01-30 11:52:40 +0100

    Added new scss and default avatar to user page

--- pages/legion/user.php
+++ pages/legion/user.php
@@ -6,8 +6,13 @@ $account->retrieveAccount();
 <div class="user_page">
 
     <h1>Welcome, <?php echo $account->getName(); ?></h1>
-    <h3>This is your user page.</h3>
 
-    <p>This is still a work in progress..</p>
+    <p>Avatar (Hover to change)</p>
+    <div class="user-avatar">
+        <div class="change-avatar">
+            <span><a href="#">Change avatar?</a></span>
+        </div>
+        <img src="../../img/avatars/no-avatar.png" alt="Avatar">
+    </div>
 
 </div>
