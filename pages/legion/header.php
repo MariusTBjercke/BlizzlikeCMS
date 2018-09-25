ac6e3e506aa6f5d704048d8f93d71237fdf507a3
Marius <marius@bjerckemedia.no>
2018-09-25 07:12:52 +0200

    (Mobile and small screens) Added a workaround for the broken links caused by the bootstrap collapse

--- pages/legion/header.php
+++ pages/legion/header.php
@@ -16,16 +16,16 @@
         <a href="index.php" title="<?php echo $servername; ?>"><div class="top-logo"></div></a>
         <div class="navbar-nav">
 
-            <button type="button" title="Open menu" id="toggleNavigation" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <button type="button" title="Open menu" id="toggleNavigation" class="navbar-toggle collapsed disabled" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                 <div class="toggledMenu">
                     <ul id="toggledMenuUl">
-                        <li class="<?php if ($page == 'home') { echo 'mobilemenu-active'; } ?>"><a href="index.php"><i class="fa fa-home" aria-hidden="true"></i> Home</a></li>
-                        <li class="<?php if ($page == 'register') { echo 'mobilemenu-active'; } ?>"><a href="register.php"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Register</a></li>
-                        <li class="<?php if ($page == 'onlineplayers') { echo 'mobilemenu-active'; } ?>"><a href="onlineplayers.php"><i class="fa fa-users" aria-hidden="true"></i> Who's online</a></li>
-                        <li class="<?php if ($page == 'forum') { echo 'mobilemenu-active'; } ?>"><a href="forum.php"><i class="fa fa-comments" aria-hidden="true"></i> Forum</a></li>
-                        <li class="<?php if ($page == 'howto') { echo 'mobilemenu-active'; } ?>"><a href="howto.php"><i class="fa fa-question-circle" aria-hidden="true"></i> How to</a></li>
-                        <li class="<?php if ($page == 'armory') { echo 'mobilemenu-active'; } ?>"><a href="armory.php"><i class="fa fa-universal-access" aria-hidden="true"></i> Armory</a></li>
-                        <li class="<?php if ($page == 'gallery') { echo 'mobilemenu-active'; } ?>"><a href="gallery.php"><i class="fa fa-file-image-o" aria-hidden="true"></i> Gallery</a></li>
+                        <li class="<?php if ($page == 'home') { echo 'mobilemenu-active'; } ?>" id="index"><a href="index.php"><i class="fa fa-home" aria-hidden="true"></i> Home</a></li>
+                        <li class="<?php if ($page == 'register') { echo 'mobilemenu-active'; } ?>" id="register"><a href="register.php"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Register</a></li>
+                        <li class="<?php if ($page == 'onlineplayers') { echo 'mobilemenu-active'; } ?>" id="onlineplayers"><a href="onlineplayers.php"><i class="fa fa-users" aria-hidden="true"></i> Who's online</a></li>
+                        <li class="<?php if ($page == 'forum') { echo 'mobilemenu-active'; } ?>" id="forum"><a href="forum.php"><i class="fa fa-comments" aria-hidden="true"></i> Forum</a></li>
+                        <li class="<?php if ($page == 'howto') { echo 'mobilemenu-active'; } ?>" id="howto"><a href="howto.php"><i class="fa fa-question-circle" aria-hidden="true"></i> How to</a></li>
+                        <li class="<?php if ($page == 'armory') { echo 'mobilemenu-active'; } ?>" id="armory"><a href="armory.php"><i class="fa fa-universal-access" aria-hidden="true"></i> Armory</a></li>
+                        <li class="<?php if ($page == 'gallery') { echo 'mobilemenu-active'; } ?>" id="gallery"><a href="gallery.php"><i class="fa fa-file-image-o" aria-hidden="true"></i> Gallery</a></li>
                     </ul>
                 </div>
                 <span class="icon-bar"></span>
--- src/js/custom/custom.js
+++ src/js/custom/custom.js
@@ -3,3 +3,10 @@ $(".editForumCategoryName").click(function() {
     var htmlString = $(".forum-category-title").text();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
     $(".forum-category-title").html('<input type="text" value="' + htmlString + '">');
 });
+
+// Nav toggle broken link fix for bootstrap
+$("#toggledMenuUl li").click(function() {
+    var linkName = $(this).attr("id");
+    var linkNameWithExt = linkName + '.php';
+    window.location.href = linkNameWithExt;
+});
