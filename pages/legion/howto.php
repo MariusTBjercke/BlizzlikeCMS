f83716b8f6f2b783d4d45df6a44aa417d4e88ebf
Marius <marius@bjerckemedia.no>
2019-02-19 15:02:26 +0100

    Fixed broken DL link var

--- pages/legion/howto.php
+++ pages/legion/howto.php
@@ -5,7 +5,7 @@
 	if (strlen($wow_downloadlink) > 0) {
 		?>
         <h3>2. If you don't have it already, download the Wrath of the Lich King client. <strong>Download link: </strong><a
-                    href="<?php echo $wotlk_downloadlink; ?>">WotLK Client (This server, fast connection.)</a></h3>
+                    href="<?php echo $wow_downloadlink; ?>">WotLK Client (This server, fast connection.)</a></h3>
         <h3>3. If you've downloaded the client from above, you can skip the next steps and <strong>open wow.exe
                 to start playing right away.</strong></h3>
         <p>or...</p>
