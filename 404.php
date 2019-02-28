03ab79887716c3e8a486607928084ef40d637352
Marius <marius@bjerckemedia.no>
2019-02-28 04:04:33 +0100

    Fixed 404 page and added mod rewrite so that extension don't have to be added to links

--- 404.php
+++ 404.php
@@ -1,8 +1,11 @@
 <?php
+session_start();
 $page = '404';
 $title = '404: ';
 include 'includes/config.php';
 include 'includes/functions.php';
+include 'includes/classes/forum_lib.php';
+include 'includes/classes/events_lib.php';
 include 'header.php';
 include 'footer.php';
 ?>
